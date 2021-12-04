7093a6b1287c67eb30251105697a7572e501bf27
Баклаулет Болат <baklauletbolat@Baklaulets-MacBook-Air.local>
2021-12-04 13:32:53 +0600

    a

--- mapbackend/settings.py
+++ mapbackend/settings.py
@@ -66,12 +66,26 @@ WSGI_APPLICATION = 'mapbackend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'map',
+            'USER' : 'bbb',
+            'PASSWORD' : '123',
+            'HOST' : '127.0.0.1',
+            'PORT' : '5432',
+        }
+    }
+
 
 
 # Password validation
